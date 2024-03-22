9f0b920b04a273ec60824b84862215473d9fb3a8
AuYang261 <459461160@qq.com>
2024-03-22 16:16:40 +0800

    use g++ instead of linking libstdc++.a manually

--- product-mini/platforms/linux/quickRun.sh
+++ product-mini/platforms/linux/quickRun.sh
@@ -1,5 +1,13 @@
 mkdir -p build && cd build && \
-    cmake .. -DCMAKE_C_COMPILER=x86_64-linux-musl-gcc -DCMAKE_CXX_COMPILER=x86_64-linux-musl-gcc -DCMAKE_AR=x86_64-linux-musl-ar -DCMAKE_RANLIB=x86_64-linux-musl-ranlib -DWAMR_BUILD_TARGET=X86_64 -DWAMR_DISABLE_HW_BOUND_CHECK=1 -DWAMR_DISABLE_WRITE_GS_BASE=1 -DWAMR_BUILD_WASI_NN=1 -DFLATBUFFERS_LOCALE_INDEPENDENT=1 \
+    cmake .. -DCMAKE_C_COMPILER=/opt/x86_64-linux-musl-cross/bin/x86_64-linux-musl-gcc \
+        -DCMAKE_CXX_COMPILER=/opt/x86_64-linux-musl-cross/bin/x86_64-linux-musl-g++ \
+        -DCMAKE_AR=/opt/x86_64-linux-musl-cross/bin/x86_64-linux-musl-ar \
+        -DCMAKE_RANLIB=/opt/x86_64-linux-musl-cross/bin/x86_64-linux-musl-ranlib \
+        -DWAMR_BUILD_TARGET=X86_64 \
+        -DWAMR_DISABLE_HW_BOUND_CHECK=1 \
+        -DWAMR_DISABLE_WRITE_GS_BASE=1 \
+        -DWAMR_BUILD_WASI_NN=1 \
+        -DFLATBUFFERS_LOCALE_INDEPENDENT=1 \
         -DCMAKE_CXX_FLAGS="-D\"strtoll_l(s, e, b, l)=strtoll(s, e, b)\" -D\"strtoull_l(s, e, b, l)=strtoull(s, e, b)\"" && \
     make -j
 
