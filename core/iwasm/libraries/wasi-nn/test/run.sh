0441d353009b9033e1b9934e01b86b73a2f9af7a
AuYang261 <459461160@qq.com>
2024-03-22 16:21:41 +0800

    modify run.sh

--- core/iwasm/libraries/wasi-nn/test/run.sh
+++ core/iwasm/libraries/wasi-nn/test/run.sh
@@ -14,5 +14,17 @@
     -o test_tensorflow_quantized.wasm \
     test_tensorflow_quantized.c utils.c
 
+/opt/wasi-sdk/bin/clang \
+    -Wl,--allow-undefined \
+    -Wl,--strip-all,--no-entry \
+    --sysroot=/opt/wasi-sdk/share/wasi-sysroot \
+    -I../include -I../src/utils \
+    -o bird.wasm \
+    bird.c utils.c
+
+cp test_tensorflow.wasm ~/ruxos/apps/c/wamr/rootfs
+cp test_tensorflow_quantized.wasm ~/ruxos/apps/c/wamr/rootfs
+cp bird.wasm ~/WasmEdge-WASINN-examples/tflite-birds_v1-image
+
 ./iwasm --env="TARGET=cpu" --dir=. test_tensorflow.wasm | grep "Tests: passed!"
 ./iwasm --env="TARGET=cpu" --dir=. test_tensorflow_quantized.wasm | grep "Tests: passed!"
